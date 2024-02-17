55f898d786f8c649bdc430b434690333ba044846
Евгений <borovoy@MacBook-Pro--Evgeniy.local>
2024-02-17 21:05:51 +0400

    change

--- blog/urls.py
+++ blog/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path("<int:year>/<int:month>/<int:day>/<slug:post>", views.post_detail, name="post_detail"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
 ]
+
