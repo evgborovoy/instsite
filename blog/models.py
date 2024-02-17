76724b7e224e2f807fdcbf5df81f2c60c54abde4
Евгений <borovoy@MacBook-Pro-Evgenij.local>
2024-02-17 10:39:49 +0400

    add pagination

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -29,6 +30,14 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=["-publish"])
         ]
+
+    def get_absolut_url(self):
+        return reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
 
     def __str__(self):
         return self.title
